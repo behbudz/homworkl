693a30d03acf4665458184b987722daeed6b5144
behbud <behbudez@code.edu.az>
2022-12-10 00:06:27 +0400

    Add project files.

---
+++ 7/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+    internal class Program
+    {
+        static void Main()
+        {
+            int a = 59;
+            int b = 24;
+
+            Console.WriteLine(a - b);
+
+            Console.ReadKey();
+        }
+    }
